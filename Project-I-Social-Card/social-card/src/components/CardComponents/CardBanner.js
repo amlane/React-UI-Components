8d226210a14f5221734c02cbc3d9433a76bfb004
amlane <twelvexstring@gmail.com>
2019-04-22 16:08:36 -0500

    more styling added to card and header

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,7 +3,9 @@ import './Card.css';
 
 const CardBanner = () => {
     return (
-        <img className="card-banner" src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" alt/>
+        <div className="card-banner">
+            <img src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" alt />
+        </div>
     )
 }
 
