ec56498166b834e80ff2dbbd97ef1b410c5906a8
amlane <twelvexstring@gmail.com>
2019-04-23 17:17:06 -0500

    refactored to get rid of unnecessary styling

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -10,42 +10,35 @@ const App = () => {
     <div className="container">
       <CalculatorDisplay />
       <div className="sub-container"> 
-
-      <div>
-        <div className="clear">
-            <ActionButton buttonStyles="wide-key" text="clear" />
+        <div>
+          <div className="clear">
+              <ActionButton buttonStyles="wide-key clear" text="clear" />
+              <ActionButton buttonStyles="action-key" text="&#247;" />
           </div>
-
-        <div className="number-keys">
-          <div className="row">
-            <NumberButton text="7" />
-            <NumberButton text="8" />
-            <NumberButton text="9" />
-          </div>
-          <div className="row"> 
-            <NumberButton text="4" />
-            <NumberButton text="5" />
-            <NumberButton text="6" />
-          </div>
-          <div className="row">
-            <NumberButton text="1" />
-            <NumberButton text="2" />
-            <NumberButton text="3" />
-          </div>
-          <div className="row">
-            <NumberButton buttonStyles="wide-key" text="0" />
-          </div>
-
-          </div>
-
-          </div>
-
-          <div className="action-keys-column">
-            <ActionButton buttonStyles="action-key" text="&#247;" />
-            <ActionButton buttonStyles="action-key" text="&#215;" />
-            <ActionButton buttonStyles="action-key" text="&#8722;" />
-            <ActionButton buttonStyles="action-key" text="&#43;" />
-            <ActionButton buttonStyles="action-key" text="&#61;" />
+          <div className="number-keys">
+              <div className="row">
+                <NumberButton text="7" />
+                <NumberButton text="8" />
+                <NumberButton text="9" />
+                <ActionButton buttonStyles="action-key" text="&#215;" />
+              </div>
+              <div className="row"> 
+                <NumberButton text="4" />
+                <NumberButton text="5" />
+                <NumberButton text="6" />
+                <ActionButton buttonStyles="action-key" text="&#8722;" />
+              </div>
+              <div className="row">
+                <NumberButton text="1" />
+                <NumberButton text="2" />
+                <NumberButton text="3" />
+                <ActionButton buttonStyles="action-key" text="&#43;" />
+              </div>
+              <div className="row">
+                <NumberButton buttonStyles="wide-key" text="0" />
+                <ActionButton buttonStyles="action-key" text="&#61;" />
+              </div>
+            </div>
           </div>
         </div>
     </div>
